58e47908284e645b7306799445a446abdace66db
Joshua <jmendel123@gmail.com>
2024-03-04 04:19:15 +0000

    Revert "Added first few requirements"
    This reverts commit 3b0ef1affc7a8e2c591987e66bfa5be97c1b4b36.

--- Java Coursework/src/main/java/uk/ac/ucl/servlets/ViewPatientListServlet.java
+++ Java Coursework/src/main/java/uk/ac/ucl/servlets/ViewPatientListServlet.java
@@ -13,36 +13,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-
-@WebServlet("/viewData.html")
-public class DataViewServlet extends HttpServlet
+// The servlet invoked to display a list of patients. Note that this data is just example data,
+// you replace it with your data.
+// The url http://localhost:8080/patientList.html is mapped to calling doGet on the servlet object.
+// The servlet object is created automatically, you just provide the class.
+@WebServlet("/patientList.html")
+public class ViewPatientListServlet extends HttpServlet
 {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
   {
     // Get the data from the model
     Model model = ModelFactory.getModel();
-
-    // Invoke the getAllPatientNames method in the model and get the data
-    String[] patientNames = model.getAllPatientNames();
-
+    List<String> patientNames = model.getPatientNames();
     // Then add the data to the request object that will be sent to the Java Server Page, so that
     // the JSP can access the data (a Java data structure).
     request.setAttribute("patientNames", patientNames);
 
-    // Get the column names from the model and add them to the request object
-    List<String> columnNames = model.getColumnNames();
-
-    request.setAttribute("columnNames", columnNames);
-
-    
-
     // Invoke the JSP.
     // A JSP page is actually converted into a Java class, so behind the scenes everything is Java.
     ServletContext context = getServletContext();
-    RequestDispatcher dispatch = context.getRequestDispatcher("/viewData.jsp");
+    RequestDispatcher dispatch = context.getRequestDispatcher("/patientList.jsp");
     dispatch.forward(request, response);
   }
 }
-
-

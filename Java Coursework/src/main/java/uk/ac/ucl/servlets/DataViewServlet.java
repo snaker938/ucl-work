a59c422c2f4875d42291a94a7e838b0de0a2e9e4
Joshua <jmendel123@gmail.com>
2024-03-04 12:58:38 +0000

    Reverted all changes

--- Java Coursework/src/main/java/uk/ac/ucl/servlets/DataViewServlet.java
+++ Java Coursework/src/main/java/uk/ac/ucl/servlets/DataViewServlet.java
@@ -13,27 +13,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-// The servlet invoked to display a list of patients. Note that this data is just example data,
-// you replace it with your data.
-// The url http://localhost:8080/patientList.html is mapped to calling doGet on the servlet object.
-// The servlet object is created automatically, you just provide the class.
-@WebServlet("/patientList.html")
-public class ViewPatientListServlet extends HttpServlet
+
+@WebServlet("/viewData.html")
+public class DataViewServlet extends HttpServlet
 {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
   {
     // Get the data from the model
     Model model = ModelFactory.getModel();
-    List<String> patientNames = model.getPatientNames();
+
+    // Invoke the getAllPatientNames method in the model and get the data
+    String[] patientNames = model.getAllPatientNames();
+
     // Then add the data to the request object that will be sent to the Java Server Page, so that
     // the JSP can access the data (a Java data structure).
     request.setAttribute("patientNames", patientNames);
 
+    // Get the column names from the model and add them to the request object
+    List<String> columnNames = model.getColumnNames();
+
+    request.setAttribute("columnNames", columnNames);
+
+    
+
     // Invoke the JSP.
     // A JSP page is actually converted into a Java class, so behind the scenes everything is Java.
     ServletContext context = getServletContext();
-    RequestDispatcher dispatch = context.getRequestDispatcher("/patientList.jsp");
+    RequestDispatcher dispatch = context.getRequestDispatcher("/viewData.jsp");
     dispatch.forward(request, response);
   }
 }
+
+
